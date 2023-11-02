bd9e414af74f60f96c1975f44122d58b9aefc461
Emmy Manning <eobcre@gmail.com>
2023-11-02 12:42:43 -0400

    connect with json server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import AddTask from './components/AddTask';
 import TodoList from './components/TodoList';
 
-
 export default function Home() {
   return (
-    <main className='flex flex-col justify-center items-center min-h-screen py-2 bg-gray-200' >
+    <main className='flex flex-col justify-center items-center min-h-screen py-2 bg-gray-200'>
       <h1 className='text-4xl font-bold text-gray-700 -mt-32'>Todo App</h1>
       <div className='w-full max-w-xl mt-5'>
         <div className='w-full px-8 py-6 bg-white shadow-md rounded-lg'>
