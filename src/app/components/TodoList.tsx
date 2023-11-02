3e210eb82ddfbe74b44755bf7896e6e460ce343d
Emmy Manning <eobcre@gmail.com>
2023-11-02 13:05:39 -0400

    todo props logic

--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import { Todo } from '@/types';
 
-const TodoList = () => {
+type TodoListProps = {
+  todos: Todo[];
+};
+
+const TodoList = ({ todos }: TodoListProps) => {
   return (
     <ul className='space-y-3'>
-      <li className='flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow'>
-        <span>Task 1</span>
-        <div>
-          <button className='text-green-500 mr-3'>Edit</button>
-          <button className='text-red-500'>Delete</button>
-        </div>
-      </li>
+      {todos.map((todo) => (
+        <li
+          key={todo.id}
+          className='flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow'
+        >
+          <span>{todo.text}</span>
+          <div>
+            <button className='text-green-500 mr-3'>Edit</button>
+            <button className='text-red-500'>Delete</button>
+          </div>
+        </li>
+      ))}
     </ul>
   );
 };
