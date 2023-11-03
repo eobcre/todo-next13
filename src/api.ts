936efc2bfff43bcfbcc8ef6fb20f2650eb8000f5
Emmy Manning <eobcre@gmail.com>
2023-11-02 20:18:24 -0400

    updated add todo logic

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,26 @@
-import { Todo } from './types';
+import { Task } from './types';
 
-export const getAllTodos = async (): Promise<Todo[]> => {
-  const res = await fetch(`http://localhost:3001/tasks`, {
+const endpoint = 'http://localhost:3001/tasks';
+
+export const getAllTodos = async (): Promise<Task[]> => {
+  const res = await fetch(endpoint, {
     cache: 'no-store',
   });
   const todos = res.json();
 
   return todos;
+};
+
+// todo add API
+export const addTodo = async (todo: Task): Promise<Task> => {
+  const res = await fetch(endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(todo),
+  });
+  const newTodo = res.json();
+
+  return newTodo;
 };
--- src/app/components/AddTask.tsx
+++ src/app/components/AddTask.tsx
@@ -1,12 +1,34 @@
-import React from 'react'
+'use client';
+
+import React, { useState, FormEvent, ChangeEvent } from 'react';
+import { addTodo } from '@/api';
 
 const AddTask = () => {
-  return (
-    <form className='mb-4 space-y-3'>
-        <input type="text" className='w-full border px-4 py-2 rounded-lg focus:outline-none focus:border-blue-400' />
-        <button className='w-full px-4 py-2 text-white bg-blue-500 rounded transform hover:bg-blue-400 hover:scale-95 duration-200'>Add Task</button>
-    </form>
-  )
-}
+  const [taskTitle, setTaskTitle] = useState('');
 
-export default AddTask
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    await addTodo({ id: '4', text: taskTitle });
+
+    setTaskTitle('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='mb-4 space-y-3'>
+      <input
+        type='text'
+        value={taskTitle}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setTaskTitle(e.target.value)
+        }
+        className='w-full border px-4 py-2 rounded-lg focus:outline-none focus:border-blue-400'
+      />
+      <button className='w-full px-4 py-2 text-white bg-blue-500 rounded transform hover:bg-blue-400 hover:scale-95 duration-200'>
+        Add Task
+      </button>
+    </form>
+  );
+};
+
+export default AddTask;
